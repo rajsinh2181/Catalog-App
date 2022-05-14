c8d75413660c3f0b7059e6a1582077dbf193620a
rajsinh2181 <rajsinh2181@gmail.com>
2022-05-14 19:14:30 +0530

    day10 theme file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/login_page.dart';
 import 'package:flutter_app/utils/routes.dart';
+import 'package:flutter_app/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
@@ -15,16 +17,9 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       // home : LoginPage(),
-      theme : ThemeData(
-        primarySwatch: Colors.blueGrey,
-        textTheme : GoogleFonts.aclonicaTextTheme(),
-      ),
-      debugShowCheckedModeBanner: false,
-      // themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-        brightness : Brightness.dark,
-        primarySwatch: Colors.lightGreen
-      ),
+      theme : MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,    
       initialRoute : MyRoutes.homeRoute,
       routes: {
         MyRoutes.loginRoute : (context) => LoginPage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,20 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 import '../widgets/drawer.dart';
 
 
 class HomePage extends StatelessWidget {
-  final double days = 3;
-   final String name = "raj";
-    final bool isMale = true;
-    final num temp = 30.5;
     
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar : AppBar(
-        title : Text("catalog"),
+        title : Text("catalog app"),
       ),
       body : Center(
         child: Container(
-            child: Text("hello $name $temp and"),
+            child: Text(""),
         ),
       ),
       drawer : MyDrawer(),
